5d7f530b8aa7606ff048127c4517d4fb3db9ca0a
chaudharykomal <161018320+chaudharykomal@users.noreply.github.com>
2024-02-23 16:14:18 +0530

    assignment2_solutions

---
+++ assignment2/ass2(4).cpp
@@ -0,0 +1,19 @@
+//WAP for finding the volume of the cylinder by taking radius and height as input
+#include<iostream>
+using namespace std;
+int main(){
+
+    int r;
+    int h;
+    float pi;
+    float v;
+    cout<<"enter the pi";
+    cin>>pi;
+    cout<<"enter the value of radius";
+    cin>>r;
+    cout<<"enter the value of height";
+    cin>>h;
+    v = pi*r*r*h ;
+  cout<<v;
+
+}
---
+++ assignment2/ass2(5).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char char1 , char2;
+    cout<<"enter the first charcter";
+    cin>>char1;
+    cout<<"enter the second character";
+    cin>>char2;
+    int diff = int(char1-char2);
+    cout<<diff;
+}
