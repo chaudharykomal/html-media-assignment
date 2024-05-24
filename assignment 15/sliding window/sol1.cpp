13b1cba53b872495b818a1f15a4ba67e83aae314
chaudharykomal <161018320+chaudharykomal@users.noreply.github.com>
2024-05-24 14:49:07 +0530

    solution of assignment 15

---
+++ assignment 15/sliding window/sol1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int numOfSubarrays(vector<int>& a, int k, int th) {
+int n = a.size();
+int sum = 0;
+int avg = 0;
+int ans = 0;
+for(int i=0;i<k;i++)sum += a[i];
+avg = sum/k;
+if(avg >= th)ans++;
+int i=k;
+while(i < n){
+sum -= a[i-k];
+sum += a[i];
+avg = sum/k;
+if(avg >= th)ans++;
+i++;
+}
+}
