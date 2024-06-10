0a256e4acc8d87bf99d51c0446711fe2df0a1cdc
chaudharykomal <161018320+chaudharykomal@users.noreply.github.com>
2024-06-10 12:17:17 +0530

    solution of linkedlist 2

---
+++ linkedList 2/sol4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+class Solution {
+public:
+ListNode* removeElements(ListNode* head, int val) {
+ListNode *curr = head;
+while (curr and curr->val == val) {
+curr = curr->next;
+}
+head = curr;
+while (curr) {
+if (curr->next and curr->next->val == val)
+curr->next = curr->next->next;
+else
+curr = curr->next;
+}
+return head;
+}
+}
+}
